237546fab5e30343ebc775d2906da7dc1e8a82bc
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2015-10-24 18:32:57 +0200

    restored

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 //Setup
-var express   = require('express');
-var https     = require('https');
-var fs        = require('fs');
-var Sequelize = require('sequelize');
-var app       = express();
-var index     = require(__dirname+'/routes/index.js');
-var port      = process.env.PORT || 4433;
+var express        = require('express');
+var https          = require('https');
+var fs             = require('fs');
+var cookieParser   = require('cookie-parser');
+var bodyParser     = require('body-parser');
+var Sequelize      = require('sequelize');
+var app            = express();
+var index          = require(__dirname+'/routes/index.js');
+var authentication = require(__dirname+'/routes/authentication.js');
+var port           = process.env.PORT || 4433;
+//ExpressSettings
+app.set('views', __dirname+'/views');
+app.set('view engine', 'jade');
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 //Database
 var db = new Sequelize('postgres://www@localhost:5432/homeserver');
 var User = db.define('users', {
@@ -43,5 +52,8 @@ var httpsOptions = {
 };
 //Routes
 app.use('/', index);
+app.use('/auth', authentication);
+//Static
+app.use(express.static(__dirname+'/public/'));
 //Start
 https.createServer(httpsOptions, app).listen(port);
