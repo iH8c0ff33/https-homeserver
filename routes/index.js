30410912aa1c4ae77159b5b40f16845d4c530093
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2015-10-26 19:28:31 +0100

    added comments

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,25 @@
 //////////////////
 var router = require('express').Router();
 
+///////
+// / //
+///////
 router.get('/', function (req, res) {
   if (!req.user) {
     return res.render('index');
   }
   res.render('index', { username: req.user.username });
 });
+////////////
+// /login //
+////////////
 router.get('/login', function (req, res) {
   if (req.user) { return res.redirect('/'); }
   res.render('login', { message: req.flash('login-error')[0] });
 });
+///////////////
+// /register //
+///////////////
 router.get('/register', function (req, res) {
   if (req.user) { return res.redirect('/'); }
   res.render('register', { error: req.flash('register-error') });
