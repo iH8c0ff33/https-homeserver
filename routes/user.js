cc841a9d6b1c0d87dc81c9b1c5ab8cc6ed4122c6
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2015-11-10 23:14:03 +0100

    working

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,29 @@ var waitSession = require(__dirname+'/../config/wait-save.js');
 
 module.exports = function (User) {
   router.use('/manage', require(__dirname+'/manage.js')(User));
-  ///////////
-  // /info //
-  ///////////
+  //////////////
+  // /account //
+  //////////////
   router.get('/account', function (req, res, next) {
     if (!req.user) { return res.redirect('/error/auth'); }
-    res.render('user/account', { user: req.user, session: req.session });
+    res.render('user/account', { user: req.user, session: req.session, error: req.flash('password-error') });
   });
+  //////////////////////
+  // /change-password //
+  //////////////////////
+  router.post('/change-password', function (req, res, next) {
+    if (!req.user) { return res.redirect('/error/auth'); }
+    var error = false;
+    if (!req.body.oldpassword) { req.flash('password-error', 'Sorry. Old password field seems to be blank.'); error = true; }
+    if (!req.body.newpassword) { req.flash('password-error', 'Sorry. New password field seems to be blank.'); error = true; }
+    if (req.body.newpassword != req.body.confirmpassword) { req.flash('password-error', 'Sorry. New passwords don\'t match'); error = true; }
+    if (error) {
+      return waitSession(req, res, next, function (err) {
+        if (err) { return next(err); }
+        res.redirect('/user/account');
+      });
+    }
+    res.send(req.body);
+  })
   return router;
 };
