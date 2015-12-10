4b5d65af64ee619094e3ab966f9a3b6107bc6350
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2015-12-10 19:46:47 +0100

    added network route and fixed
    Added a route for the network monitor and now sending user as argument for jade render

--- routes/monitor.js
+++ routes/monitor.js
@@ -3,8 +3,16 @@
 ////////////////////
 var router = require('express').Router();
 var waitSession = require(__dirname+'/../config/wait-save.js');
-
+///////////
+// /raid //
+///////////
 router.get('/raid', function (req, res, next) {
-  res.render('monitor/raid');
+  res.render('monitor/raid', { user: req.user });
+});
+//////////////
+// /network //
+//////////////
+router.get('/network', function (req, res, next) {
+  res.render('monitor/network', { user: req.user });
 });
 module.exports = router;
