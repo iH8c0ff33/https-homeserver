ef359e7b6299223d1c11e15c597b728537fedf4c
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2015-12-12 12:56:32 +0100

    now secret key is secret...

--- config/session.js
+++ config/session.js
@@ -1,15 +1,17 @@
 ////////////////////
 // Session config //
 ////////////////////
+var fs = require('fs');
+
 module.exports = function (sessionStore) {
   var sessionConfig = {
-    secret: 'stograncazzo',
+    secret: fs.readFileSync(__dirname+'/../secret.key').toString('utf8'),
     cookie: {
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 30
     },
     resave: false, //Don't enable (will break with sequelize)
-    saveUninitialized: true, //Need to enable to use flashes
+    saveUninitialized: true, //Need to be enabled to use flashes
     store: sessionStore
   }
   return sessionConfig;
